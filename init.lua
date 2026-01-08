e84ee43d8f80608f01910b837518c761b2ee9f4d
Alex Gherghisan <alexghr@users.noreply.github.com>
2026-01-08 22:59:50 +0000

    feat: mini.pick

--- init.lua
+++ init.lua
@@ -1,4 +1,14 @@
-vim.g.alexghr_autoformat = true
+vim.pack.add {
+  { src = 'https://github.com/neovim/nvim-lspconfig' },
+  { src = 'https://github.com/nvim-treesitter/nvim-treesitter' },
+  { src = 'https://github.com/stevearc/oil.nvim' },
+  { src = 'https://github.com/tpope/vim-fugitive' },
+  { src = 'https://github.com/folke/tokyonight.nvim' },
+  { src = 'https://github.com/nvim-mini/mini.pick' },
+  { src = 'https://github.com/nvim-mini/mini.icons.git' },
+}
+
+vim.cmd([[colorscheme tokyonight]])
 
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
@@ -8,6 +18,7 @@ vim.opt.mouse = 'a'
 vim.opt.clipboard = 'unnamedplus'
 
 vim.opt.signcolumn = 'yes'
+vim.opt.winborder = 'rounded'
 
 vim.opt.undofile = true
 vim.opt.updatetime = 250
@@ -16,12 +27,17 @@ vim.opt.timeoutlen = 300
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.opt.expandtab = true
 vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 vim.opt.cursorline = true
 vim.opt.scrolloff = 10
 vim.opt.breakindent = true
+vim.opt.wrap = true
 
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
@@ -34,221 +50,19 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+require('mini.icons').setup()
 
-vim.keymap.set('n', '<leader>ce', ':e $MYVIMRC<CR>', { desc = '[E]dit [c]onfig' })
-vim.keymap.set('n', '<leader>cs', ':source $MYVIMRC<CR>', { desc = '[E]dit [c]onfig' })
-
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
-vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
-
-vim.keymap.set('n', 'bd', '<CMD>bd<CR>', { desc = 'Close current buffer' })
-vim.keymap.set('n', 'bda', '<CMD>%bd<CR>', { desc = 'Close all buffers' })
-
-function TermToggle(height)
-  if term_win and vim.api.nvim_win_is_valid(term_win) then
-    vim.api.nvim_set_current_win(term_win)
-    vim.cmd 'hide'
-  else
-    vim.cmd 'botright new'
-    local new_buf = vim.api.nvim_get_current_buf()
-    vim.cmd('resize ' .. height)
-    if term_buf and vim.api.nvim_buf_is_valid(term_buf) then
-      vim.cmd('buffer ' .. term_buf) -- go to terminal buffer
-      vim.cmd('bd ' .. new_buf) -- cleanup new buffer
-    else
-      vim.cmd 'terminal'
-      term_buf = vim.api.nvim_get_current_buf()
-      vim.wo.number = false
-      vim.wo.relativenumber = false
-      vim.wo.signcolumn = 'no'
-    end
-    vim.cmd 'startinsert!'
-    term_win = vim.api.nvim_get_current_win()
-  end
-end
-
-vim.keymap.set('n', '<Leader>`', ':lua TermToggle(20)<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<Leader><Esc>', ':lua TermToggle(20)<CR>', { noremap = true, silent = true })
-vim.keymap.set('t', '<Leader>`', '<C-\\><C-n>:lua TermToggle(20)<CR>', { noremap = true, silent = true })
-vim.keymap.set('t', '<Leader><Esc>', '<C-\\><C-n>:lua TermToggle(20)<CR>', { noremap = true, silent = true })
-
-vim.pack.add{
-  { src = 'https://github.com/neovim/nvim-lspconfig' },
-
-  { src = 'https://github.com/nvim-lua/plenary.nvim' },
-  { src = 'https://github.com/nvim-telescope/telescope.nvim' }, -- depends on plenary
-  { src = 'https://github.com/nvim-treesitter/nvim-treesitter' },
-
-  { src = 'https://github.com/stevearc/conform.nvim' },
-  { src = 'https://github.com/stevearc/oil.nvim' },
-
-  { src = 'https://github.com/lewis6991/gitsigns.nvim' },
-
-  { src = 'https://github.com/mfussenegger/nvim-lint' },
-
-  { src = 'https://github.com/tpope/vim-sleuth' },
-  { src = 'https://github.com/tpope/vim-fugitive' },
-
-  { src = 'https://github.com/folke/tokyonight.nvim' },
-  { src = 'https://github.com/folke/which-key.nvim' },
-}
-
-vim.cmd([[colorscheme tokyonight]])
-
-local servers = {
-  lua_ls = {},
-  ols = {},
-  rust_analyzer = {},
-}
-
-for server, config in pairs(servers) do
-  vim.lsp.config(server, config)
-  vim.lsp.enable(server)
-end
-
-local builtin = require 'telescope.builtin'
-vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-
--- Slightly advanced example of overriding default behavior and theme
-vim.keymap.set('n', '<leader>/', function()
-  -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-  builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 10,
-    previewer = false,
-  })
-end, { desc = '[/] Fuzzily search in current buffer' })
-
--- It's also possible to pass additional configuration options.
---  See `:help telescope.builtin.live_grep()` for information about particular keys
-vim.keymap.set('n', '<leader>s/', function()
-  builtin.live_grep {
-    grep_open_files = true,
-    prompt_title = 'Live Grep in Open Files',
+local MiniPick = require('mini.pick')
+MiniPick.setup({
+  mappings = {
+    execute = {
+      char = '<C-e>',
+      func = function() vim.cmd(vim.fn.input('Execute: ')) end,
+    }
   }
-end, { desc = '[S]earch [/] in Open Files' })
-
--- Shortcut for searching your Neovim configuration files
-vim.keymap.set('n', '<leader>sn', function()
-  builtin.find_files { cwd = vim.fn.stdpath 'config' }
-end, { desc = '[S]earch [N]eovim files' })
-
-
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
-  callback = function(event)
-    -- NOTE: Remember that Lua is a real programming language, and as such it is possible
-    -- to define small helper and utility functions so you don't have to repeat yourself.
-    --
-    -- In this case, we create a function that lets us more easily define mappings specific
-    -- for LSP related items. It sets the mode, buffer and description for us each time.
-    local map = function(keys, func, desc, mode)
-      mode = mode or 'n'
-      vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
-    end
-
-    -- Jump to the definition of the word under your cursor.
-    --  This is where a variable was first declared, or where a function is defined, etc.
-    --  To jump back, press <C-t>.
-    map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-
-    -- Find references for the word under your cursor.
-    map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-
-    -- Jump to the implementation of the word under your cursor.
-    --  Useful when your language has ways of declaring types without an actual implementation.
-    map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-
-    -- Jump to the type of the word under your cursor.
-    --  Useful when you're not sure what type a variable is and you want to see
-    --  the definition of its *type*, not where it was *defined*.
-    map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-
-    -- Fuzzy find all the symbols in your current document.
-    --  Symbols are things like variables, functions, types, etc.
-    map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-
-    -- Fuzzy find all the symbols in your current workspace.
-    --  Similar to document symbols, except searches over your entire project.
-    map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-    -- Rename the variable under your cursor.
-    --  Most Language Servers support renaming across files, etc.
-    map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-
-    -- Execute a code action, usually your cursor needs to be on top of an error
-    -- or a suggestion from your LSP for this to activate.
-    map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
-
-    -- WARN: This is not Goto Definition, this is Goto Declaration.
-    --  For example, in C this would take you to the header.
-    map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-
-    map('sh', vim.lsp.buf.signature_help, '[S]ignature [H]elp')
-
-    vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers['signature_help'], {
-      border = 'single',
-      close_events = { 'CursorMoved', 'BufHidden', 'InsertCharPre' },
-    })
-
-    -- The following two autocommands are used to highlight references of the
-    -- word under your cursor when your cursor rests there for a little while.
-    --    See `:help CursorHold` for information about when this is executed
-    --
-    -- When you move your cursor, the highlights will be cleared (the second autocommand).
-    local client = vim.lsp.get_client_by_id(event.data.client_id)
-    if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-      local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
-      vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-        buffer = event.buf,
-        group = highlight_augroup,
-        callback = vim.lsp.buf.document_highlight,
-      })
-
-      vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-        buffer = event.buf,
-        group = highlight_augroup,
-        callback = vim.lsp.buf.clear_references,
-      })
-
-      vim.api.nvim_create_autocmd('LspDetach', {
-        group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
-        callback = function(event2)
-          vim.lsp.buf.clear_references()
-          vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
-        end,
-      })
-    end
-
-    -- The following code creates a keymap to toggle inlay hints in your
-    -- code, if the language server you are using supports them
-    --
-    -- This may be unwanted, since they displace some of your code
-    if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-      map('<leader>th', function()
-        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
-      end, '[T]oggle Inlay [H]ints')
-    end
-  end,
 })
 
-require("oil").setup({
+require('oil').setup({
   default_file_explorer = true,
   columns = {
     'icon',
@@ -259,3 +73,48 @@ require("oil").setup({
     show_hidden = true,
   },
 })
+
+require('lua.lsp_config')
+require('lua.term_toggle')
+
+vim.keymap.set('n', '<leader>`', ':lua TermToggle(20)<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader><ESC>', ':lua TermToggle(20)<CR>', { noremap = true, silent = true })
+vim.keymap.set('t', '<leader>`', '<C-\\><C-n>:lua TermToggle(20)<CR>', { noremap = true, silent = true })
+vim.keymap.set('t', '<leader><ESC>', '<C-\\><C-n>:lua TermToggle(20)<CR>', { noremap = true, silent = true })
+
+vim.keymap.set('n', '-', ':Oil<CR>', { desc = 'Open parent directory' })
+
+vim.keymap.set('n', '<leader>sf', MiniPick.builtin.files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sg', MiniPick.builtin.grep_live, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>sr', MiniPick.builtin.resume, { desc = '[S]earch [R]esume' })
+vim.keymap.set('n', '<leader>q',
+  function() MiniPick.start({ source = { name = 'Diagnostics', items = vim.diagnostic.toqflist(vim.diagnostic.get()) } }) end,
+  { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<leader><leader>', MiniPick.builtin.buffers, { desc = '[ ] Find existing buffers' })
+
+vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, { desc = '[F]ormat buffer' })
+
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+vim.keymap.set('n', '<leader>ec', '<CMD>e $MYVIMRC<CR>', { desc = '[E]dit [c]onfig' })
+vim.keymap.set('n', '<leader>r', ':source $MYVIMRC<CR>', { desc = '[R]eload config' })
+
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+vim.keymap.set('i', '<C-space>', '<C-x><C-o>', { desc = 'Autocomplete' })
+
+-- Enable LSP complete
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(event)
+    local client = vim.lsp.get_client_by_id(event.data.client_id)
+    if client and client:supports_method('textDocument/completion') then
+      vim.lsp.completion.enable(true, client.id, event.buf, { autotrigger = true })
+    end
+  end
+})
+
+vim.cmd([[ set completeopt+=noselect ]])
